7b9e0d803e835851d21a036df372497e0627e731
AzarovDN <azarovdn@icloud.com>
2018-12-14 16:15:49 +1000

    почистил код, сделал список вывода, не сделал запись в файл

--- diplom.py
+++ diplom.py
@@ -1,9 +1,7 @@
 import requests
 
-# token = 'ed1271af9e8883f7a7c2cefbfddfcbc61563029666c487b2f71a5227cce0d1b533c4af4c5b888633c06ae'
 
 # id = input('Введите имя пользователя или его id: ')
-
 
 # id = '171691064' #  Шмаргунов
 
@@ -14,7 +12,6 @@ id = 'another_nastyaf'
 token = 'ed1271af9e8883f7a7c2cefbfddfcbc61563029666c487b2f71a5227cce0d1b533c4af4c5b888633c06ae'
 
 class User:
-    token = 'ed1271af9e8883f7a7c2cefbfddfcbc61563029666c487b2f71a5227cce0d1b533c4af4c5b888633c06ae'
 
     def __init__(self, user_id):
         self.user_id = user_id
@@ -35,49 +32,37 @@ else:
 user = User(id)
 
 params = {
-    'access_token': user.token,
+    'access_token': token,
     'v': '5.92',
     'user_id': user.user_id
 }
 
 friends = requests.get('https://api.vk.com/method/friends.get', params)  # Получил json друзей
-params = {
-    'access_token': user.token,
-    'v': '5.92',
-    'user_id': user.user_id,
-    'count': '1000'
-}
-
 my_groups = requests.get('https://api.vk.com/method/groups.get', params)  # Получил json групп
 
-# print(friends.json())
-
 friends_list = friends.json()['response']['items']  # получил список друзей
-# print(f'Cписок моих друзей {friends_list}')
-# print(len(friends_list))
-
 my_group_list = my_groups.json()['response']['items']  # получил список групп
 
-print('Мои группы: ', my_group_list, '\n')
+print('Не забудь удалить! Мои группы: ', my_group_list, '\n')
 
 all_friends_group = []
 
-try:
-    for friend in friends_list:
+n = 0
 
-        params = {
-            'access_token': user.token,
-            'v': '5.92',
-            'user_id': friend
-        }
-
+for friend in friends_list:
+    params = {
+        'access_token': token,
+        'v': '5.92',
+        'user_id': friend
+    }
+    try:
         friend_group = requests.get('https://api.vk.com/method/groups.get', params)
         friend_group_list = friend_group.json()['response']['items']
-
         all_friends_group.extend(friend_group_list)
-except KeyError:
-        # print('Ошибка в группе', friend_group.json())
-        pass
+    except KeyError:
+        continue
+
+
 print(len(all_friends_group))
 
 all_friends_group_set = set(all_friends_group)
@@ -85,9 +70,24 @@ all_friends_group_set = set(all_friends_group)
 print(len(all_friends_group_set))
 
 
-result = set(my_group_list) - all_friends_group_set
+results = set(my_group_list) - all_friends_group_set
+
+out_data = []
+for group_id in results:
+    params = {
+        'access_token': token,
+        'v': '5.92',
+        'group_id': group_id
+    }
+    group_info = requests.get('https://api.vk.com/method/groups.getById', params)
+    # print(group_info.json())
+    group_mem = requests.get('https://api.vk.com/method/groups.getMembers', params)
+    # print(group_mem.json()['response']['count'])
+    group_info_dict = {
+        'name': group_info.json()['response'][0]['name'],
+        'gid': group_info.json()['response'][0]['id'],
+        'members_count': group_mem.json()['response']['count']
+    }
+    out_data.append(group_info_dict)
 
 
-print(f'Результат: {result}')
-
-print(len(result))
