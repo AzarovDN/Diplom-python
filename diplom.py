f4c7a7abc97e31c8a3aa52534b0562c940bcad48
AzarovDN <azarovdn@icloud.com>
2018-12-15 11:08:14 +1000

    сделал индикацию обработки файлов и групп

--- diplom.py
+++ diplom.py
@@ -1,13 +1,16 @@
 import requests
+import json
+import time
+import sys
 
 
 # id = input('Введите имя пользователя или его id: ')
 
-# id = '171691064' #  Шмаргунов
+id = '171691064' #  Шмаргунов
 
-# id = '171691064'  # Азаров
+# id = '9897521'  # Азаров
 
-id = 'another_nastyaf'
+# id = '230412273'
 
 token = 'ed1271af9e8883f7a7c2cefbfddfcbc61563029666c487b2f71a5227cce0d1b533c4af4c5b888633c06ae'
 
@@ -17,9 +20,12 @@ class User:
         self.user_id = user_id
 
 
+def backspace():
+    print('\r', end='')
+
+
 if id.isdigit():
     id = id
-    print('1')
 else:
     params = {
         'access_token': token,
@@ -43,18 +49,26 @@ my_groups = requests.get('https://api.vk.com/method/groups.get', params)  # По
 friends_list = friends.json()['response']['items']  # получил список друзей
 my_group_list = my_groups.json()['response']['items']  # получил список групп
 
-print('Не забудь удалить! Мои группы: ', my_group_list, '\n')
 
 all_friends_group = []
 
-n = 0
+counter = len(friends_list)
 
 for friend in friends_list:
+
+    counter -= 1
+
+    backspace()
+    s = f'Осталось обработать {counter} друзей'  # string for output
+    sys.stdout.write(s)
+    # time.sleep(0.2)  # sleep for 200ms
+
     params = {
         'access_token': token,
         'v': '5.92',
         'user_id': friend
     }
+
     try:
         friend_group = requests.get('https://api.vk.com/method/groups.get', params)
         friend_group_list = friend_group.json()['response']['items']
@@ -62,32 +76,43 @@ for friend in friends_list:
     except KeyError:
         continue
 
+print('\n')
 
-print(len(all_friends_group))
+results = set(my_group_list) - set(all_friends_group)
 
-all_friends_group_set = set(all_friends_group)
-
-print(len(all_friends_group_set))
-
-
-results = set(my_group_list) - all_friends_group_set
 
 out_data = []
+counter = len(results)
+
 for group_id in results:
+
+    counter -= 1
+
+    backspace()
+    s = f'Осталось обработать {counter} групп'  # string for output
+    sys.stdout.write(s)
+    # time.sleep(0.2)  # sleep for 200ms
+
     params = {
         'access_token': token,
         'v': '5.92',
         'group_id': group_id
     }
-    group_info = requests.get('https://api.vk.com/method/groups.getById', params)
-    # print(group_info.json())
-    group_mem = requests.get('https://api.vk.com/method/groups.getMembers', params)
-    # print(group_mem.json()['response']['count'])
-    group_info_dict = {
-        'name': group_info.json()['response'][0]['name'],
-        'gid': group_info.json()['response'][0]['id'],
-        'members_count': group_mem.json()['response']['count']
-    }
-    out_data.append(group_info_dict)
+    try:
+        group_info = requests.get('https://api.vk.com/method/groups.getById', params)
+        group_members = requests.get('https://api.vk.com/method/groups.getMembers', params)
+        group_info_dict = {
+            'name': group_info.json()['response'][0]['name'],
+            'gid': group_info.json()['response'][0]['id'],
+            'members_count': group_members.json()['response']['count']
+        }
+        out_data.append(group_info_dict)
+    except KeyError:
+        print(f'Ошибка тут {group_info.json()}')
+        continue
 
+with open('groups.json', 'w', encoding='utf-8') as f:
+    json.dump(out_data, f, ensure_ascii=False)
 
+print('\n')
+print('данные записаны в файл')
