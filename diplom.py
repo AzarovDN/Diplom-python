1bf36a7237cea85deb5a70a925339a4ee7c79307
AzarovDN <azarovdn@icloud.com>
2018-12-21 14:23:29 +1000

    Перенес параметры в json, почти закончил обработку исключений.

--- diplom.py
+++ diplom.py
@@ -2,55 +2,93 @@ import requests
 import json
 import time
 import sys
-from user_class import User
+
+
+class User:
+
+    def __init__(self, user_id):
+        self.user_id = user_id
 
 
 def backspace():
+
     print('\r', end='')
 
 
-def default_params():
+def take_params():
 
-    default_params = {
-        'access_token': token,
-        'v': '5.92',
-    }
-    return default_params
+    with open('params.json', encoding='utf-8') as f:  # заменить имя файла на params_export, поменять token в файле
+        default_params = json.load(f)[0]
+        return default_params
 
 
-def made_id(id):
+def made_id(id, default_params):
 
     if id.isdigit():
         id = id
     else:
-
-        params = default_params()
+        params = default_params
         params['user_ids'] = id
-        id_json = requests.get('https://api.vk.com/method/users.get', params)
-        id = id_json.json()['response'][0]['id']
+
+        try:
+            id_json = requests.get('https://api.vk.com/method/users.get', params)
+            id = id_json.json()['response'][0]['id']
+
+        except KeyError:
+            if id_json.json()['error']['error_code'] == 5:
+                print(f"В работе программы возникла ошибка {id_json.json()['error']['error_msg']}")
+                print('Программа перезапущенна!!!')
+                work_program()
+            elif id_json.json()['error']['error_code'] == 113:
+                print('Введите корректное имя пользователя')
+                print('Программа перезапущена!!!')
+                work_program()
+            else:
+                print('Ошибка', id_json.json()['error']['error_msg'])
+                print('Программа перезапущена!!!')
+                work_program()
+
+        except NameError:
+            if id_json.json()['error']['error_code'] == 5:
+                print(f"В работе программы возникла ошибка {id_json.json()['error']['error_msg']}")
+                print('Введите корректное имя пользователья. Программа перезапущена')
+                work_program()
+            else:
+                print('Ошибка', id_json.json()['error']['error_msg'])
+                print('Программа перезапущена!!!')
+                work_program()
+
     return id
 
 
-def user_data():
+def user_data(default_params, user):
 
     friends = '"friends": API.friends.get({' + '"user_id":' + f'{user.user_id}' + '})'
     my_groups = '"my_groups": API.groups.get({' + '"user_id":' + f'{user.user_id}' + '})'
 
     code = 'return {' + f'{friends}, {my_groups}' + '};'
 
-    params = default_params()
+    params = default_params
     params['code'] = code
+    try:
+        response = requests.get('https://api.vk.com/method/execute?', params)
+        friends_list = response.json()['response']['friends']['items']
+        my_group_list = response.json()['response']['my_groups']['items']
 
-
-    response = requests.get('https://api.vk.com/method/execute?', params)
-
-    friends_list = response.json()['response']['friends']['items']
-    my_group_list = response.json()['response']['my_groups']['items']
+    except KeyError:
+        if response.json()['error']['error_code'] == 5:
+            print(f"В работе программы возникла ошибка {response.json()['error']['error_msg']}")
+            print('Программа перезапущенна!!!')
+            work_program()
+        else:
+            print('Ошибка', response.json()['error']['error_msg'])
+            print('Программа перезапущена!!!')
+            work_program()
 
     return friends_list, my_group_list
 
 
-def find_secret_group(friends_list, my_group_list):
+def find_secret_group(friends_list, my_group_list,default_params):
 
     all_friends_group = []
     counter = len(friends_list)
@@ -58,17 +96,15 @@ def find_secret_group(friends_list, my_group_list):
     for friend in friends_list:
 
         counter -= 1
-
         backspace()
 
         s = f'Осталось обработать {counter} друзей'
         sys.stdout.write(s)
-        # time.sleep(0.1)
 
         friend_group = '"friend_group": API.groups.get({' + '"user_id":' + f'{friend}' + '})'
         code = 'return {' + f'{friend_group}' + '};'
 
-        params = default_params()
+        params = default_params
         params['code'] = code
 
         try:
@@ -76,20 +112,26 @@ def find_secret_group(friends_list, my_group_list):
             friend_group_list = response.json()['response']['friend_group']['items']
             all_friends_group.extend(friend_group_list)
         except TypeError:
-            print('TypeError в find_secret_group')
+            if response.json()['execute_errors'][0]['error_code'] == 30:
+                print('\n', f'Закрытый доступ в профиль {friend}')
             continue
         except KeyError:
-            print('KeyError в find_secret_group')
-            continue
+            if response.json()['error']['error_code'] == 6:
+                time.sleep(0.5)
+                response = requests.get('https://api.vk.com/method/execute?', params)
+                friend_group_list = response.json()['response']['friend_group']['items']
+                all_friends_group.extend(friend_group_list)
+                continue
+            else:
+                print('Ошибка', response.json()['error']['error_msg'])
 
     print('\n')
-
     results = set(my_group_list) - set(all_friends_group)
 
     return results
 
 
-def find_friend_in_group(n, groups, friend):
+def find_friend_in_group(n, groups, friend, default_params):
 
     all_mutural_group_list = []
     counter = len(groups)
@@ -101,11 +143,10 @@ def find_friend_in_group(n, groups, friend):
         backspace()
         s = f'Осталось обработать {counter} групп'
         sys.stdout.write(s)
-        # time.sleep(0.1)
         mutural = '"mutural_list": API.groups.getMembers({' + '"group_id":' + f'{group}' + '})'
         code = 'return {' + f'{mutural},' + '};'
 
-        params = default_params()
+        params = default_params
         params['code'] = code
 
         try:
@@ -114,10 +155,16 @@ def find_friend_in_group(n, groups, friend):
                 counter_group += 1
                 all_mutural_group_list.append(group)
         except KeyError:
-            print('KeyError в find_friend_in_group')
-            continue
+            if mutural_list.json()['error']['error_code'] == 6:
+                time.sleep(0.5)
+                mutural_list = requests.get('https://api.vk.com/method/execute?', params).json()['response']['mutural_list']['items']
+                if len(set(friend) & set(mutural_list)) > 0 & len(set(friend) & set(mutural_list)) <= n:
+                    counter_group += 1
+                    all_mutural_group_list.append(group)
+                continue
         except TypeError:
-            print('TypeError в find_friend_in_group')
+            if mutural_list.json()['execute_errors'][0]['error_code'] == 30:
+                print('\n', f'Закрытый доступ в профиль {friend}')
             continue
         except AttributeError:
             print('AttributeError в find_friend_in_group')
@@ -127,8 +174,7 @@ def find_friend_in_group(n, groups, friend):
     return all_mutural_group_list
 
 
-
-def write_file(writes_file):
+def write_file(writes_file, default_params):
 
     out_data =[]
     counter = len(writes_file)
@@ -137,19 +183,19 @@ def write_file(writes_file):
 
         counter -= 1
 
+
         backspace()
         s = f'Осталось записать {counter} групп'
         sys.stdout.write(s)
-        # time.sleep(0.1)
 
         group_info = '"group_info": API.groups.getById({' + '"group_id":' + f'{group_id}' + '})'
         group_members = '"group_members": API.groups.getMembers({' + '"group_id":' + f'{group_id}' + '})'
 
         code = 'return {' + f'{group_info}, {group_members}' + '};'
 
-        params = default_params()
+        params = default_params
         params['code'] = code
-        
+
         try:
             response = requests.get('https://api.vk.com/method/execute?', params)
             group_info_dict = {
@@ -159,7 +205,14 @@ def write_file(writes_file):
             }
             out_data.append(group_info_dict)
         except KeyError:
-            print(f'Ошибка тут {response.json()} в write_file')
+            if response.json()['error']['error_code'] == 6:
+                time.sleep(0.5)
+                response = requests.get('https://api.vk.com/method/execute?', params)
+                group_info_dict = {
+                    'name': response.json()['response']['group_info'][0]['name'],
+                    'gid': response.json()['response']['group_info'][0]['id'],
+                    'members_count': response.json()['response']['group_members']['count']
+                }
             continue
 
     with open('groups.json', 'w', encoding='utf-8') as f:
@@ -168,38 +221,61 @@ def write_file(writes_file):
     print('данные записаны в файл')
 
 
-if __name__ == '__main__':
+def search_for_secret_groups(default_params, user):
+    friends_list, my_group_list = user_data(default_params, user)
+    result = find_secret_group(friends_list, my_group_list, default_params)
+    write_file(result, default_params)
 
-    token = 'ed1271af9e8883f7a7c2cefbfddfcbc61563029666c487b2f71a5227cce0d1b533c4af4c5b888633c06ae'
 
+def search_for_common_groups(default_params,user):
+    n = int(input('N - максимальное количество друзей в группе. Введите N: '))
+    friends_list, my_group_list = user_data(default_params,user)
+    result = find_friend_in_group(n, my_group_list, friends_list, default_params)
+    write_file(result, default_params)
+
+
+def work_program():
+    default_params = take_params()
     id = input('Введите id пользователя или id: ')
-    # id = input('Введите имя пользователя или его id: ')
-    # id = '171691064'  # Шмаргунов
-    # id = '9897521'  # Азаров
-    # id = '230412273'  # В этом id всего 25 друзей
-
-    user = User(made_id(id))
-
+    user = User(made_id(id, default_params))
     print('Поиск секретных групп пользователя - 1')
     print('Поиск общих групп с друзьями - 2')
     what_find = input('Выберите режим поиска: ')
 
     if what_find == '1':
-        friends_list, my_group_list = user_data()
-        result = find_secret_group(friends_list, my_group_list)
-        write_file(result)
+        search_for_secret_groups(default_params, user)
     elif what_find == '2':
-        n = int(input('N - максимальное количество друзей в группе. Введите N: '))
-        friends_list, my_group_list = user_data()
-        result = find_friend_in_group(n, my_group_list, friends_list)
-        write_file(result)
+        search_for_common_groups(default_params, user)
     else:
         print('Вы ввели некорректные данные')
 
-    # user = User(230412273)
-    # friends_list, my_group_list = user_data()
-    # result = find_friend_in_group(10, my_group_list, friends_list)
-    # write_file(result)
+
+if __name__ == '__main__':
+    work_program()
+    # default_params = take_params()
+    #
+    # id = input('Введите id пользователя или id: ')
+    # # id = input('Введите имя пользователя или его id: ')
+    # # id = '171691064'  # Шмаргунов
+    # # id = '9897521'  # Азаров
+    # # id = '230412273'  # В этом id всего 25 друзей
+    #
+    # user = User(made_id(id))
+    #
+    # print('Поиск секретных групп пользователя - 1')
+    # print('Поиск общих групп с друзьями - 2')
+    # what_find = input('Выберите режим поиска: ')
+    #
+    # if what_find == '1':
+    #     search_for_secret_groups()
+    # elif what_find == '2':
+    #     search_for_common_groups()
+    # else:
+    #     print('Вы ввели некорректные данные')
+    #
+    # # user = User(230412273)
+    # # search_for_secret_groups()
+
 
 
 
